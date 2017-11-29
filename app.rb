8252efab189ea3321c592f6ea752f10027b64bb5
Erika Barr <EB@erikas-mbp.attlocal.net>
2017-11-29 11:32:27 -0600

    add route

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'open-uri'
+require 'json'
 
 get '/' do
+	@tweets = {'data': 'enter a twitter handle please'}
   haml :index
 end
 
+get '/analyzer' do
+  params[:twitterHandle] ||= 'need to set instance variable in controller'				
+  @tweets =	{'data': "100 analyzed tweets for #{params[:twitterHandle]}"}
+	haml :index
+end
---
+++ public/helper.js
@@ -0,0 +1,3 @@
+function getSentiment(twitterHandle) {
+  alert('helper file works') ;	
+};
