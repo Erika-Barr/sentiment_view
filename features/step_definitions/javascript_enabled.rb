3938a11b82d33d7b4efb78e08e618d1efc7b49fa
Erika Barr <EB@erikas-mbp.home>
2018-01-25 16:18:45 -0600

    rename cuke steps

---
+++ features/step_definitions/javascript_enabled.rb
@@ -0,0 +1,8 @@
+Given /^I am on the index page$/ do
+				  visit "/"
+end
+Then /^I should see chart loaded about "(@.*)"$/ do |handle|
+				page.assert_selector('.highcharts-anchor') 
+				page.assert_text(:visible, "Chart loaded about @tastytrade")
+end
+
